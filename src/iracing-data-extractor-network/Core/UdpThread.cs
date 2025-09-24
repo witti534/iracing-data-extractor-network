884246841dbf5bea57161b5d360be77d403a9fde
Alexander Wittmann <19347984+witti534@users.noreply.github.com>
2025-09-24 23:10:17 +0200

    Added function to send UDP packages

---
+++ src/iracing-data-extractor-network/Core/UdpThread.cs
@@ -0,0 +1,37 @@
+﻿using Data;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace Core;
+
+/// <summary>
+/// This class is responsible for containing the thread which sends UDP packages.
+/// </summary>
+public class UdpThread
+{
+    public UdpThread(string address, int port, BasicDataModel data, CancellationToken cancellationToken)
+    {
+        _ipEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        _data = data;
+        _cancellationToken = cancellationToken;
+        _udpClient = new();
+    }
+
+    public void ThreadLoop()
+    {
+        while (!_cancellationToken.IsCancellationRequested)
+        {
+            byte[] jsonStringBytes = JsonSerializer.SerializeToUtf8Bytes(_data);
+
+            _udpClient.Send(jsonStringBytes, jsonStringBytes.Length, _ipEndPoint);
+
+            Thread.Sleep(60);
+        }
+    }
+
+    private IPEndPoint _ipEndPoint;
+    private BasicDataModel _data;
+    private CancellationToken _cancellationToken;
+    private UdpClient _udpClient;
+}
